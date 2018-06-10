42ee3e565255ebb7b35471a2a9c4c8e957c19454
Bartosz Legięć <bartosz@legiec.eu>
2018-06-11 00:12:35 +0200

    Fix deployment

--- test.js
+++ test.js
@@ -19,9 +19,6 @@ execute
     console.log("\x1b[32m%s\x1b[0m", "✔ All test passed!");
   })
   .catch((err) => {
-    console.log("\x1b[32m%s\x1b[0m", "✔ All test passed!");
-    console.log(err);
-
-    // console.error("\x1b[31m%s\x1b[0m", "✘ Test failed – " + err.message);
-    // process.exit(1);
+    console.error("\x1b[31m%s\x1b[0m", "✘ Test failed – " + err.message);
+    process.exit(1);
   });
