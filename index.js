6df0eefb589e787c2b328d107dac42368265046b
Bartosz Legięć <bartosz@legiec.eu>
2018-06-10 22:33:01 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-// {% codepen userId|anonymous|anon slugHash theme [defaultTab [height [width]]] %}
+// {% codepen slugHash defaultTabs height width userId|anonymous|anon theme %}
+
 function codepenTagRender(args) {
-  const userId = args[0] === 'anonymous' ? 'anon' : args[0];
-  const slugHash = args[1];
-  const theme = args[2];
-  const defaultTab = args[3] != null ? args[3] : 'result';
-  const height = args[4] != null ? args[4] : 300;
-  const width = args[5] != null ? args[5] : '100%';
-  const src = `//codepen.io/${userId}/embed/${slugHash}?height=${height}&theme-id=${theme}&slug-hash=${slugHash}&default-tab=${defaultTab}`;
+  const [
+    hash,
+    tabs = hexo.config.codepen.default_tabs || "result,js",
+    height = hexo.config.codepen.height || 300,
+    width = hexo.config.codepen.width || "100%",
+    user,
+    theme = hexo.config.codepen.theme || "dark"
+  ] = args;
+
+  const userId = user ? user === 'anonymous' ? 'anon' : user : hexo.config.codepen.userId || "anon";
+
+  if ( !hash ) {
+    return new Error("Pen hash should be provided");
+  }
+
+  const src = `//codepen.io/${userId}/embed/${slugHash}?height=${height}&theme-id=${theme}&slug-hash=${slugHash}&default-tab=${defaultTab}&embed-version=2`;
   const id = `cp_embed_${slugHash.replace(/\//g, '_')}`
 
   return `<iframe id="${id}" src="${src}" scrolling="no" frameborder="no" height="${height}" allowTransparency="true" allowfullscreen="true" class="cp_embed_iframe" style="width: ${width}; overflow: hidden;"></iframe>`;
